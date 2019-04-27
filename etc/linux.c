cd6bf4540d8d6ba4caccc06bf99fe7bc03330e18
koyamanX <ckoyama.1996@gmail.com>
2019-04-27 19:10:49 +0900

    fix path

--- etc/linux.c
+++ etc/linux.c
@@ -6,9 +6,10 @@ static char rcsid[] = "$Id$";
 
 #define CPPDIR "/bin/"
 #define BINUTILSDIR "/opt/sun32-toolchain/binutils/bin"
-#define LCCDIR "/opt/sun32-toolchain/lcc"
+#define LCCDIR "/opt/sun32-toolchain/lcc/bin"
 #define LIBDIR "/opt/sun32-toolchain/lib"
 #define INCDIR "/opt/sun32-toolchain/include"
+#define PREFIX "sun32-unknown-elf-"
 
 char *suffixes[] = { ".c", ".i", ".s", ".o", ".out", 0 };
 char inputs[256] = "";
@@ -19,9 +20,9 @@ char *cpp[] = { CPPDIR "cpp",
 	"$1", "$2", "$3", 0 };
 char *include[] = {"-I"INCDIR, 0 };
 char *com[] = {LCCDIR"/rcc", "-target=sun", "$1", "$2", "$3", 0 };
-char *as[] = {BINUTILSDIR"/as", "-o", "$3", "$1", "$2", 0 };
+char *as[] = {BINUTILSDIR"/"PREFIX"as", "-o", "$3", "$1", "$2", 0 };
 char *ld[] = {
-	/*  0 */ BINUTILSDIR"/ld", "", "", "-static",
+	/*  0 */ BINUTILSDIR"/"PREFIX"ld", "", "", "-static",
 	/*  4 */ "", "-o", "$3",
 	/*  7 */ "", "",
 	/*  9 */ "", 
